835b4e42e1d0852a9725fd4749902b64537220b7
Benjamin Lee <benjaminjlee10@gmail.com>
2023-04-26 17:20:12 -0400

    Initial Commit

---
+++ Voto/VotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VotoApp.swift
+//  Voto
+//
+//  Created by Benjamin Lee on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct VotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
