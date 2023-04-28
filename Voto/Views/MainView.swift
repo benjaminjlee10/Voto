34f08362cccb0845fbc6ce6f5b178796be0e1c13
Benjamin Lee <benjaminjlee10@gmail.com>
2023-04-28 01:31:47 -0400

    added daily timer to MainView

--- Voto/Views/MainView.swift
+++ Voto/Views/MainView.swift
@@ -19,7 +19,11 @@ struct MainView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Text("\(currentTime.hourAndMinute)")
+                Text(countdownString())
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
                 
                 List {
                     ForEach(uploads) { upload in
@@ -44,11 +48,38 @@ struct MainView: View {
                 }
             }
         }
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                updateTime()
+            }
+        }
         .sheet(isPresented: $sheetIsPresented) {
             NavigationStack {
                 UploadView(upload: Upload())
             }
         }
+    }
+    
+    private func updateTime() {
+        let calendar = Calendar.current
+        let endOfDay = calendar.date(bySettingHour: 23, minute: 59, second: 59, of: Date())!
+        let startOfTomorrow = calendar.date(byAdding: .day, value: 1, to: endOfDay)!
+        let interval = endOfDay.timeIntervalSince(currentTime)
+        if interval <= 0 {
+            currentTime = startOfTomorrow
+        } else {
+            currentTime = Date()
+        }
+    }
+    
+    private func countdownString() -> String {
+        let calendar = Calendar.current
+        let endOfDay = calendar.date(bySettingHour: 23, minute: 59, second: 59, of: Date())!
+        let interval = endOfDay.timeIntervalSince(currentTime)
+        let hours = Int(interval) / 3600
+        let minutes = Int(interval) / 60 % 60
+        let seconds = Int(interval) % 60
+        return String(format: "Time Remaining: %02d:%02d:%02d", hours, minutes, seconds)
     }
 }
 
